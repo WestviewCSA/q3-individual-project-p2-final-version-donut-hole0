af26a3d53b1839e7e1738cdea9fc2b5148689985
alant <alant@Alan4L>
2026-03-22 09:37:57 -0700

    Finished solveOptimal()

--- Q3 Project 1 Period 2/src/p1.java
+++ Q3 Project 1 Period 2/src/p1.java
@@ -449,5 +449,156 @@ public class p1{
 	}
 	
 	
+	// same code as solveQueue() but it solves in less time thank solveQueue() + solveStack()
+	public void solveOptimal() {
+		int rows   = map.length;
+	    int cols   = map[0].length;
+	    int levels = map[0][0].length;
+	    
+	    int[] dRow = {-1, 1, 0, 0};
+	    int[] dCol = { 0, 0, 1,-1};
+	    
+	    int startRow = -1;
+	    int startCol = -1;
+	    int startLevel = 0;
+	    boolean found = false;
+	    
+	    
+	    for(int level = 0; level < levels; level++) {
+	    	for(int row = 0; row < rows; row++) {
+	    		for(int col = 0; col < cols; col++) {
+	    			if(!found && map[row][col][level] == 'W') {
+	    				startRow = row;
+	    				startCol = col;
+	    				startLevel = level;
+	    				found = true;
+	    			}
+	    			
+	    		}
+	    	}
+	    }
+	    
+	    int[][][][] sol = new int[rows][cols][levels][3];
+	    
+	    // use -1 as symbol for not visited yet
+	    for(int r = 0; r < rows; r++) {
+	    	for(int c = 0; c < cols; c++) {
+	    		for(int l = 0; l < levels; l++) {
+	    			sol[r][c][l][0] = -1;
+	    			sol[r][c][l][1] = -1;
+	    			sol[r][c][l][2] = -1;
+	    		}
+	    	}
+	    }
+	    
+	    boolean[][][] visited = new boolean[rows][cols][levels];
+	    
+	    Queue<int[]> queue = new LinkedList<int[]>();
+	    
+	    visited[startRow][startCol][startLevel] = true;
+	    
+	    int[] start = {startRow, startCol, startLevel};
+	    
+	    queue.add(start);
+	    
+	    
+	    int[] goal = null;
+	    
+	    while(!queue.isEmpty()) {
+	    	int[] current = queue.remove();
+	    	int row = current[0];
+	    	int col = current[1];
+	    	int level = current[2];
+	    	
+	    	// checking north, south, east, west
+	    	
+	    	for(int d = 0; d < 4; d++) {
+	    		int nRow = row + dRow[d];
+	    		int nCol = col + dCol[d];
+	    		int nLevel = level;
+	    		
+	    		// checks if out of bounds
+	    		if (nRow < 0 || nRow >= rows || nCol < 0 || nCol >= cols) {
+	                continue;
+	            }
+	    		
+	    		
+	    		// checks visited
+	    		if (visited[nRow][nCol][nLevel]) {
+	                continue;
+	            }
+	    		
+	    		
+	    		if (map[nRow][nCol][nLevel] == '@') {
+	                continue;
+	            }
+	    		
+	    		if (map[nRow][nCol][nLevel] == '|') {
+	    			int nextLevel = nLevel + 1;
+	    		    if (nextLevel >= levels) continue;
+	    		    
+	    		    boolean foundW = false;
+	    		    for (int r = 0; r < rows && !foundW; r++) {
+	    		    	for (int c = 0; c < cols && !foundW; c++) {
+	    		    		if (map[r][c][nextLevel] == 'W') {
+	    		    			if (!visited[r][c][nextLevel]) {
+	    		    				visited[r][c][nextLevel] = true;
+	    		    				sol[r][c][nextLevel][0] = row;
+	    		    				sol[r][c][nextLevel][1] = col;
+	    		    				sol[r][c][nextLevel][2] = level;
+	    		    				int[] v = {r, c, nextLevel};
+	    		    				queue.add(v);
+	    		    				}
+	    		    			foundW = true;
+	    		    			}
+	    		    		}
+	    		    	}
+	    		    continue; 
+	            }
+	    		
+	    		visited[nRow][nCol][nLevel] = true;
+	    		sol[nRow][nCol][nLevel][0] = row;
+	    		sol[nRow][nCol][nLevel][1] = col;
+	    		sol[nRow][nCol][nLevel][2] = level;
+	    		
+	    		int[] v = {nRow, nCol, nLevel};
+	    		queue.add(v);
+	    		
+	    		if (map[nRow][nCol][nLevel] == '$') {
+	                goal = new int[]{nRow, nCol, nLevel};
+	                break;
+	            }
+
+	    	}
+	    	
+	    	if (goal != null) {
+	            break;
+	        }
+	    }
+	    
+	    if (goal == null) {
+	    	System.out.println("The Wolverine Store is closed.");
+	        return;
+	    }
+	    
+	    int[] curr = goal;
+
+	    while (true) {
+	    	int[] p = sol[curr[0]][curr[1]][curr[2]];
+	    	
+	    	if (p[0] == -1) {
+	            break;
+	        }
+	    	
+	    	if (map[p[0]][p[1]][p[2]] != 'W') {
+	            map[p[0]][p[1]][p[2]] = '+';
+	        }
+	    	
+	    	curr= p;
+	    }
+	    printMap();
+	    
+	}
+	
 }
 
